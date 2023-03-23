4ea76db006aa1af7a2cff4a893e03d9e72db9f0c
Joshua Pollack <jpollack@gmail.com>
2023-03-23 13:34:53 -0700

    adds another one off

---
+++ mfac/gharchive_split.cpp
@@ -0,0 +1,68 @@
+#include "docopt.h"
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cstdio>
+#include <cstring>
+#include <getopt.h>
+#include <iomanip>
+#include <iostream>
+#include <jansson.h>
+#include <map>
+#include <memory>
+#include <openssl/sha.h>
+#include <readosm.h>
+#include <sstream>
+#include <stdexcept>
+#include <stdint.h>
+#include <string>
+#include <sys/time.h>
+#include <vector>
+#include <unordered_map>
+#include <nlohmann/json.hpp>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+using json = nlohmann::json;
+
+inline void __dieunless (const char *msg, const char *file, int line) { fprintf (stderr, "[%s:%d] Assertion '%s' failed.\n", file, line, msg); abort (); }
+
+static uint64_t usec_now (void) { return chrono::duration_cast<chrono::microseconds>(chrono::system_clock::now().time_since_epoch()).count(); }
+static const char USAGE[] =
+    R"(usage: gharchive_split [options] 
+
+options:
+  -h --help
+  --prefix=STRING        Prefix [default: type_]
+
+)";
+
+
+int main (int argc, char **argv) 
+{ 
+    auto d{docopt::docopt (USAGE,{argv+1,argv+argc})};
+    string opf = d["--prefix"].asString ();
+    
+    unordered_map<string, ofstream*> fhm;
+
+    string line;
+    ofstream *osp{nullptr};
+
+    while (getline (std::cin, line)) {
+	json j = json::parse (line);
+	const string& tstr = j["type"];
+	auto s = fhm.find (tstr);
+	if (s == fhm.end ()) {	    
+	    fhm.emplace (tstr, osp = (new ofstream(opf + tstr + ".ndjson")));
+	} else {
+	    osp = s->second;
+	}
+	*osp << line << "\n";
+    }
+
+    for (auto& [k, v]: fhm)	v->close ();
+	
+    return 0;
+}
